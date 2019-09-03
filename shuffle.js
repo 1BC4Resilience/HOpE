7256f67a1d0107ac92fd683a958d3396c87a4012
IanOD <36891240+IanOD@users.noreply.github.com>
2019-09-03 13:19:37 +0200

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -163,7 +163,7 @@ function getMarkupFromData(dataForSingleItem) {
   var validated = dataForSingleItem[7];
   
   return [  
-      '<figure class="col-3@xs col-4@sm col-3@md picture-item" data-groups=///'["' + tags + ']///' ',
+      '<figure class="col-3@xs col-4@sm col-3@md picture-item" data-groups=//'["' + tags + ']//' ',
       'data-date-created="' + dateCreated + '" ',
       'data-title="' + title +'">',
       '<div class="picture-item__inner">',
