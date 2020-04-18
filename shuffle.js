b79ed61f5f1a703645118293f7262349b326c7bb
IanOD <36891240+IanOD@users.noreply.github.com>
2020-04-18 20:21:20 +0200

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -254,7 +254,7 @@ function appendMarkupToPage(markup) {
 var visible = true;
 setInterval(function(){
     document.getElementById('Intro_text1').style.display = visible ? 'none' : 'block'; // use short if/else to decide which value to user
-    document.getElementById('IntroText2').style.display    = visible ? 'block' : 'none'; // short if/else is called ternairy
+    document.getElementById('Intro_text2').style.display    = visible ? 'block' : 'none'; // short if/else is called ternairy
     visible = !visible; // reverse the value of itself
 }, 3000);
  
